6ffba48fb89b3cb4eb76e4a821ba37a8ed76fb4b
火花✨ <61813396+Hibanaw@users.noreply.github.com>
2022-02-19 14:33:26 +0800

    Update changefavicon.js

--- changefavicon.js
+++ changefavicon.js
@@ -34,6 +34,7 @@ $(document).ready(function(){
             if(changed){
                 changeFavicon('/favicon.jpg');
                 document.title = '才没有寂寞呢(๑‾᷅^‾᷅๑)';
+                changed = false;
                 setTimeout(function() {
                     document.title = originTitle;
                 }, 3000);
